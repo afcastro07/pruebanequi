c4e4d3dc6f0a4655c7776c8a0aa648f3ebb710ab
afcastro07 <acastrop5@ucentral.edu.co>
2024-09-30 19:05:53 -0500

    Enviados Correcciones

--- src/main/java/co/nequi/proyectofranquicias/controlador/ControladorFranquicia.java
+++ src/main/java/co/nequi/proyectofranquicias/controlador/ControladorFranquicia.java
@@ -2,7 +2,6 @@ package co.nequi.proyectofranquicias.controlador;
 
 import co.nequi.proyectofranquicias.entidades.Franquicia;
 import co.nequi.proyectofranquicias.servicios.ServiciosFranquicia;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
